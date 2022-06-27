a922c890bf320caf18fab20f6a8e6d72109a5987
damskw <damian.skwierawski@gmail.com>
2022-06-27 14:25:38 +0200

    Board creation + display option for one board and two side by side

--- battleship.py
+++ battleship.py
@@ -1,13 +1,15 @@
-
-
-
+from board import create_board, display_two_boards
 from menu import show_logo
+
 
 
 filenames = ["logo.txt"]
 
 def main():
   show_logo(filenames)
+  board1 = create_board(5)
+  board2 = create_board(5)
+  display_two_boards(board1, board2, 5)
 
 
 
---
+++ board.py
@@ -0,0 +1,84 @@
+def create_board(board_size):
+  clean_board = []
+  for size in range(board_size):
+    clean_board.append(["0"] * board_size)
+  return clean_board
+
+
+def dictionary():
+  dictionary = {0: "A", 1: "B", 2: "C", 3: "D", 4:"E", 5:"F", 6: "G", 7: "H"}
+  return dictionary
+
+
+def display_board(board, board_size):
+  show_column_numbers("   ", board_size)
+  print("\r")
+  show_rows_for_one_board(board, board_size)
+
+
+def show_column_numbers(blank, board_size):
+  show_space = True
+  for number in range(board_size):
+    if show_space:
+      print(blank + str(number + 1), end = "  ")
+      show_space = False
+    else:
+      print(str(number + 1), end = "  ")
+
+
+def show_rows_for_one_board(board, board_size):
+    numbers_to_letters = dictionary()
+    column = 0
+    new_row = True
+    
+    for row in range(board_size):
+      letter = numbers_to_letters[row]
+
+      while column != board_size - 1:
+        if new_row:
+          print(letter + "  " + board[row][column], end = " ")
+          new_row = False
+        else:
+          print(" " + board[row][column], end = " ")
+          column += 1
+      new_row = True
+      print("\r")
+      column = 0
+      new_row = True
+
+
+def display_two_boards(board1, board2, board_size):
+  show_column_numbers("   ", board_size)
+  show_column_numbers("    ", board_size)
+  print("\r")
+  show_rows_for_two_boards(board1, board2, board_size)
+
+
+def show_rows_for_two_boards(board1, board2, board_size):
+    numbers_to_letters = dictionary()
+    column_board_1 = 0
+    column_board_2 = 0
+    new_row = True
+    for row in range(board_size):
+      letter = numbers_to_letters[row]
+
+      while column_board_1 != board_size - 1:
+        if new_row:
+          print(letter + "  " + board1[row][column_board_1], end = " ")
+          new_row = False
+        else:
+          print(" " + board1[row][column_board_1], end = " ")
+          column_board_1 += 1
+      new_row = True
+
+      while column_board_2 != board_size - 1:
+        if new_row:
+          print("  " + letter + "  " + board2[row][column_board_2], end = " ")
+          new_row = False
+        else:
+          print(" " + board2[row][column_board_2], end = " ")
+          column_board_2 += 1
+      print("\r")
+      column_board_1 = 0
+      column_board_2 = 0
+      new_row = True
